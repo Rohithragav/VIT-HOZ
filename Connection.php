08ed13dafaa1d51cb81cf291e9fee2921982d5fa
ROHITH <rohithragav77@gmail.com>
2017-09-05 13:27:11 +0530

    Add files via upload

---
+++ Connection.php
@@ -0,0 +1,9 @@
+<?php 
+$_servername = "localhost";
+$_username = "root";
+$_password = "";
+$_db = "vit-hoz";
+// Create connection
+$conn = new mysqli($_servername, $_username, $_password, $_db);
+
+?>
---
+++ Feed.php
@@ -0,0 +1,85 @@
+<?php
+$today=Date("Y/m/d");
+echo $today;
+/**
+*
+*/
+class Feeds 
+{
+	function __construct($feedname,$feedDesc,$feedimage,$date,$block)
+	{
+		# INSERT INTO `hostelproj`.`feeds` (`fid`, `name`, `desc`, `img`, `expdate`, `block`) VALUES ('1', 'dff', 'fdfsfs', 'df', '23/3/2017', 'A');
+
+		require 'Connection.php';
+		if(!$conn->connect_error){
+			$sql = "INSERT INTO feeds (`name`, `desc`, `img`, `expdate`, `block`) VALUES ('$feedname', '$feedDesc', '$feedimage', '$date', '$block');";
+			$result=$conn->query($sql);
+
+		}
+		else{
+			echo $conn->connect_error;
+		}
+		mysqli_close($conn);
+	}
+
+	public function getFeedsByBlock($block)
+	{
+		require 'Connection.php';
+		if(!$conn->connect_error){
+			$sql = "SELECT * FROM feeds where block='".$block."';";
+			$result=$conn->query($sql);
+			$row = array();
+			while ($row[]=$result->fetch_assoc()) {}
+			echo json_encode($row);
+		}
+		else{
+			echo $conn->connect_error;
+		}
+		mysqli_close($conn);
+	}
+
+	public function getFeedsById($fid)
+	{
+		require 'Connection.php';
+		if(!$conn->connect_error){
+			$sql = "SELECT * FROM feeds where fid='$fid';";
+			$result=$conn->query($sql);
+			$row=$result->fetch_assoc();
+			echo json_encode($row);
+		}
+		else{
+			echo $conn->connect_error;
+		}
+		mysqli_close($conn);
+	}
+
+	public function getTodayFeeds($date)
+	{
+		require 'Connection.php';
+		if(!$conn->connect_error){
+			$sql = "SELECT * FROM feeds where expdate='$date';";
+			$result=$conn->query($sql);
+			$row=$result->fetch_assoc();
+			echo json_encode($row);
+		}
+		else{
+			echo $conn->connect_error;
+		}
+		mysqli_close($conn);
+	}
+
+	public function deleteFeed($fid)
+	{
+		# DELETE FROM `hostelproj`.`feeds` WHERE `fid`='1';
+		require 'Connection.php';
+		if(!$conn->connect_error){
+			$sql = "DELETE FROM feeds WHERE fid='$fid';";
+			$result=$conn->query($sql);
+		}
+		else{
+			echo $conn->connect_error;
+		}
+		mysqli_close($conn);
+	}
+}
+?>

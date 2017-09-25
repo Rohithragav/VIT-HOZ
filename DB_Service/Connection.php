757185c576dc295dccf513aec4a762492cce86c2
ROHITH <rohithragav77@gmail.com>
2017-09-25 21:50:23 +0530

    Delete Connection.php

--- DB_Service/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$_servername = "localhost";
-$_username = "root";
-$_password = "";
-$_db = "vit-hoz";
-// Create connection
-$conn = new mysqli($_servername, $_username, $_password, $_db);
-
-?>
